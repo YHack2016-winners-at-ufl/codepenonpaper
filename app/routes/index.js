8c297e900fa6f3f2abf5f6648cac58cd4c60fbdc
Matt Lemmone <mattlemmone@Matts-MacBook-Pro.local>
2016-11-12 00:57:29 -0500

    because public is basically empty?

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('home', { body: 'Express' });
+    res.render('home', { body: 'example' });
 });
 
 module.exports = router;
