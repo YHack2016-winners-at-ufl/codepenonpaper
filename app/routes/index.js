107e53cffe283ac8928910ec168c591dd2c4b552
Matt Lemmone <mattlemmone@Matts-MacBook-Pro.local>
2016-11-12 02:02:38 -0500

    basic layout done

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('home', { body: 'example' });
+    res.render('home', {
+        output: {
+            javascript: 'var msg = "hello world"; alert(msg);',
+            html: '<h2 id="#exout">example html output</h2>',
+            css: '#exout {font-size: 50px;color:red}'
+        }
+    });
 });
 
 module.exports = router;
